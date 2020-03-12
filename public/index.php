712318416d4a162956586e3213a8125c0a0e67e5
Anton Samoylenko <a.samoylenko95@gmail.com>
2020-03-12 13:12:09 +0200

    Refactor index.php

--- public/index.php
+++ public/index.php
@@ -13,11 +13,18 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 // Don't do it in production, assembling takes it's time
 Builder::rebuild();
-
-$container = new Container(require Builder::path('web'));
-
 require_once dirname(__DIR__) . '/src/globals.php';
 
+$definitions = (require Builder::path('web'));
+$providers = [];
+
+$debugEnabled = (bool)($params['debugger.enabled'] ?? false) && class_exists(Debugger::class);
+if ($debugEnabled) {
+    $providers[] = new DebugServiceProvider();
+    $providers[] = new \Yiisoft\Yii\Debug\Viewer\DebugViewerServiceProvider();
+}
+
+$container = new Container($definitions, $providers);
 $application = $container->get(Application::class);
 
 $request = $container->get(ServerRequestFactory::class)->createFromGlobals();
